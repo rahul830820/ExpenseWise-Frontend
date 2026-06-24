a94957e846515f41dbe3e6d89a755f2ff10864e1
Rahul Kamble <rahulkamble@Rahuls-MacBook-Air.local>
2026-06-24 19:26:22 +0530

    Complete expense CRUD and dashboard integration

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,6 +1,70 @@
+"use client"
+
+import { useEffect, useState } from "react"
 import Sidebar from "@/components/sidebar"
 
 export default function DashboardPage() {
+
+  const [totalIncome, setTotalIncome] =
+    useState(0)
+
+  const [totalExpense, setTotalExpense] =
+    useState(0)
+
+  useEffect(() => {
+    fetchDashboardData()
+  }, [])
+
+  const fetchDashboardData = async () => {
+  try {
+    const token =
+      localStorage.getItem("access_token")
+
+    const incomeResponse = await fetch(
+      `${process.env.NEXT_PUBLIC_API_URL}/incomes`,
+      {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      }
+    )
+
+    const expenseResponse = await fetch(
+      `${process.env.NEXT_PUBLIC_API_URL}/expenses`,
+      {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      }
+    )
+
+    const incomeData =
+      await incomeResponse.json()
+
+    const expenseData =
+      await expenseResponse.json()
+
+    const incomeTotal =
+      incomeData.items.reduce(
+        (sum: number, income: any) =>
+          sum + income.amount,
+        0
+      )
+
+    const expenseTotal =
+      expenseData.items.reduce(
+        (sum: number, expense: any) =>
+          sum + expense.amount,
+        0
+      )
+
+    setTotalIncome(incomeTotal)
+    setTotalExpense(expenseTotal)
+
+  } catch (error) {
+    console.error(error)
+  }
+}
   return (
     <div className="flex min-h-screen bg-slate-100">
       <Sidebar />
@@ -17,7 +81,7 @@ export default function DashboardPage() {
             </h2>
 
             <p className="text-2xl font-bold">
-              ₹1,25,000
+              ₹{(totalIncome - totalExpense).toLocaleString()}
             </p>
           </div>
 
@@ -27,7 +91,7 @@ export default function DashboardPage() {
             </h2>
 
             <p className="text-2xl font-bold text-green-600">
-              ₹80,000
+              ₹{totalIncome.toLocaleString()}
             </p>
           </div>
 
@@ -37,7 +101,7 @@ export default function DashboardPage() {
             </h2>
 
             <p className="text-2xl font-bold text-red-600">
-              ₹35,000
+              ₹{totalExpense.toLocaleString()}
             </p>
           </div>
         </div>
